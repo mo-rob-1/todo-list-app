f89ee3e96ee5a47f2af2c7393a263ef4126bf79f
Mo Rob <31892724+morob1@users.noreply.github.com>
2021-02-10 15:28:08 +0000

    added todo list functionality

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,22 +1,69 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TodoList extends Component {
+import './TodoList.css';
+
+class TodoList extends React.Component {
     constructor(props) {
-        super(props);
-
-        this.state={
-            newItem: "",
-            list: []
-        }
+      super(props);
+      this.state = {
+        newItem: "",
+        list: []
+      };
     }
-
-    render () {
-        return (
-            <div className="TodoList">
-                
-            </div>
-        );
+  
+    updateInput(key, value) {
+      // update react state
+      this.setState({ [key]: value });
     }
-}
+  
+    addItem() {
+      // create a new item with unique id
+      const newItem = {
+        id: 1 + Math.random(),
+        value: this.state.newItem.slice()
+      };
+  
+      // copy current list of items
+      const list = [...this.state.list];
+  
+      // add the new item to the list
+      list.push(newItem);
+  
+      // update state with new list, reset the new item input
+      this.setState({
+        list,
+        newItem: ""
+      });
+    }
+  
+    deleteItem(id) {
+      // copy current list of items
+      const list = [...this.state.list];
+      // filter out the item being deleted
+      const updatedList = list.filter(item => item.id !== id);
+  
+      this.setState({ list: updatedList });
+    }
+    
+    render() {
+      return (
+          <div className="TodoList">
+            <input type="text" placeholder="Add Item" value={this.state.newItem} onChange={e => this.updateInput("newItem", e.target.value)}/>
+            <button onClick={() => this.addItem()} disabled={!this.state.newItem.length}>+</button>
 
-export default TodoList;
+            <ul>
+              {this.state.list.map(item => {
+                return (
+                  <li key={item.id}>
+                    {item.value}
+                    <button onClick={() => this.deleteItem(item.id)}>x</button>
+                  </li>
+                );
+              })}
+            </ul>
+        </div>
+      );
+    }
+  }
+  
+  export default TodoList;
